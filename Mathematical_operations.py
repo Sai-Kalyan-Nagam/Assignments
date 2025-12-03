063cfe174e6bba0d3d71da62b101c98debca68bb
Sai-Kalyan-Nagam <saikalyan.nagam@gmail.com>
2025-12-03 22:22:45 +0530

    Assignment-1

---
+++ Mathematical_operations.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print("Addition: ", sum)
+diff = b - a
+print("Subtraction: ", diff)
+multiply = a * b
+print("Multiplication: ", multiply)
+divide = a / b
+print("Division: ", divide)
